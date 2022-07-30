669297b4e48e976d7d6dcf55262e73d13466f255
nikit <nikit@NLZH-LAPTOP>
2022-07-30 14:24:46 +0400

    Добавьте файлы проекта.

---
+++ fileProccesing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileProccesing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            Console.WriteLine("Hello, world!" + counter);
+        }
+    }
+}
