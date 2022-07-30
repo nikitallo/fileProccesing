64b9fe80a0670d2b0f8e04e1c14a20bbff47f345
NLozhkin <nikital99@mail.ru>
2022-07-30 15:18:18 +0400

    add code to compare file info for 2 files

--- fileProccesing/Program.cs
+++ fileProccesing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,26 @@ namespace fileProccesing
     {
         static void Main(string[] args)
         {
-            int counter = 1;
-            Console.WriteLine("Hello, world!" + counter);
+            string path1 = @"C:\myLab\fileProccesing\fileProccesing\source\textFileLink";
+            string path2 = @"C:\myLab\fileProccesing\fileProccesing\source\textFile.txt";
+
+            var file1 = new FileInfo(path1);
+            var file2 = new FileInfo(path2);
+
+            if (file1.Exists && file2.Exists) {
+
+                Console.WriteLine($"name: \n {file1.Name} \n {file2.Name}" );
+                Console.WriteLine($"creation time: \n {file1.CreationTime} \n {file2.CreationTime}");
+                Console.WriteLine($"full name: \n {file1.FullName} \n {file2.FullName}");
+                Console.WriteLine($"length: \n {file1.Length} \n {file2.Length}");
+                Console.WriteLine($"attributes: \n {file1.Attributes} \n {file2.Attributes}");
+                Console.WriteLine($"creation time: \n {file1.CreationTime} \n {file2.CreationTime}");
+                Console.WriteLine($"last access time: \n {file1.LastAccessTime} \n {file2.LastAccessTime}");
+                Console.WriteLine($"last write time: \n {file1.LastWriteTime} \n {file2.LastWriteTime}");
+                Console.WriteLine($"Extension: \n {file1.Extension} \n {file2.Extension}");
+               file1.
+            }
+           
         }
     }
 }
